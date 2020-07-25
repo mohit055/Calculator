ad75d6097bf83df49c2a99e0bfd5ef9480d08800
mohit055 <bloodhead055@gmail.com>
2020-07-25 13:27:25 +0530

    Initial Publish

---
+++ Source/script.js
@@ -0,0 +1,17 @@
+function calc() {
+  var a = parseInt(document.querySelector("#a").value);
+  var b = parseInt(document.querySelector("#b").value);
+  var op = document.querySelector("#op").value;
+  var result;
+  if (op == "Addition") {
+    result = a + b;
+  } else if (op == "Subtraction") {
+    result = a - b;
+  } else if (op == "Multiplication") {
+    result = a * b;
+  } else if (op == "Division") {
+    result = a / b;
+  }
+
+  document.querySelector("#result").innerHTML = result;
+}
